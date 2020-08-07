6e190250afa19267a15808bae6c7d8d62ef33873
df <df@dfdeMacBook-Pro.local>
2020-08-07 23:23:21 +0800

    init

---
+++ Example/Pods/Target Support Files/HelloXXXPod/HelloXXXPod-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_HelloXXXPod : NSObject
+@end
+@implementation PodsDummy_HelloXXXPod
+@end
---
+++ HelloXXXPod/Classes/UIImage+circle.h
@@ -0,0 +1,18 @@
+//
+//  UIImage+circle.h
+//  Expecta
+//
+//  Created by df on 7/8/20.
+//
+
+
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIImage (circle)
+
+@end
+
+NS_ASSUME_NONNULL_END
